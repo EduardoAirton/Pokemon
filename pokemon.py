711cb548d45a307259e0e2bd48a8f3b0a39415c0
Eduardo Airton <eduardoairton03@gmail.com>
2018-05-10 18:01:00 -0300

    Add files via upload
    Atualizando o arquivo.

--- pokemon.py
+++ pokemon.py
@@ -18,28 +18,102 @@ class Pokemons:
     def adicionar_pontos(self):
         self.pontos += 1
 
+    def atacar(self):
+        print("1 - Ataque 1\n2 - Ataque 2\n3 - Defender\n4 - ESPECIAL\n")
+        ataque = input()
+        return ataque
+
+class Artes:
+    def pokemon(self):
+        print("                                                                                                              ")
+        print("                                                                          , \                                ")
+        print("                                            _.----.        ____         ,'  _\   ___    ___     ____       \n")
+        print("                                        _,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`. \n")
+        print("                                        \      __    \    '-.  | /   `.  ___    |    \/    |   '-.   \ |  |\n")
+        print("                                         \.    \ \   |  __  |  |/    ,','_  `.  |          | __  |    \|  |\n")
+        print("                                           \    \/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |\n")
+        print("                                            \     ,-'/  /   \    ,'   | \/ / ,`.|         /  /   \  |     |\n")
+        print("                                             \    \ |   \_/  |   `-.  \    `'  /|  |    ||   \_/  | |\    |\n")
+        print("                                              \    \ \      /       `-.`.___,-' |  |\  /| \      /  | |   |\n")
+        print("                                               \    \ `.__,'|  |`-._    `|      |__| \/ |  `.__,'|  | |   |\n")
+        print("                                                \_.-'       |__|    `-._ |              '-.|     '-.| |   |\n")
+        print("                                                                        `'                            '-._|\n")
+    def pokemon2(self):
+        print("        ;-.               ,\n")
+        print("        \ '.           .'/\n")
+        print("         \  \ .---. .-' /\n")
+        print("          '. '     `\_.'\n")
+        print("            |(),()  |     ,\n")
+        print("            (  __   /   .' \n")
+        print("           .''.___.'--,/\_,|\n")
+        print("          {  /     \   }   |\n")
+        print("           '.\     /_.'    /\n")
+        print("            |'-.-',  `; _.'\n")
+        print("            |  |  |   |`\n")
+        print("            `\"\"`\"\"`\"\"\"`  \n")
+    def pokemon3(self):
+        print("              .-. \_/ .-.\n")
+        print("               \.-\/=\/.-/\n")
+        print("            '-./___|=|___\.-'\n")
+        print("           .--| \|/`\"`\/ |--.\n")
+        print("          (((_)\  .---.  /(_)))\n")
+        print("           `\ \_`-.   .-'_/ /`_\n")
+        print("             '.__       __.'(_))\n")
+        print("                 /     \     //\n")
+        print("                |       |__.'/\n")
+        print("                \       /--'`\n")
+        print("            .--,-' .--. '----.\n")
+        print("           '----`--'  '--`----'\n")
+
+
+
 class pokemonFogo(Pokemons):
     def __init__(self, nome):
         super().__init__("Charizard")
-        self.ATAQUE1 = 10
-        self.ATAQUE2 = 20
-        self.ATAQUE3 = 35
+        self.tipo = "Fogo"
+        self.FORTE1 = "Grama"
+        self.FRACO1 = "Agua"
+        self.FRACO2 = "Fogo"
+        self.arte = Artes.fogo;
 
 class pokemonAgua(Pokemons):
     def __init__(self, nome):
         super().__init__("Blastoise")
+        self.tipo = "Agua"
+        self.FORTE1 = "Fogo"
+        self.FRACO1 = "Agua"
+        self.FRACO2 = "Grama"
 
 class pokemonEletrico(Pokemons):
     def __init__(self, nome):
         super().__init__("Electabuzz")
+        self.tipo = "Eletrico"
+        self.FORTE1 = "Agua"
+        self.FRACO1 = "Fogo"
+        self.FRACO2 = "Grama"
 
 class pokemonGrama(Pokemons):
     def __init__(self, nome):
         super().__init__("Vileplume")
+        self.tipo = "Grama"
+        self.FORTE2 = "Gelo"
+        self.FRACO1 = "Agua"
+        self.FRACO2 = "Fogo"
 
 class pokemonGelo(Pokemons):
     def __init__(self, nome):
         super().__init__("Jynx")
+        self.tipo = "Gelo"
+        self.FORTE1 = "Grama"
+        self.FRACO1 = "Fogo"
+        self.FRACO2 = "Agua"
+
+if __name__ == '__main__':
+    poke = Artes()
+    poke.pokemon3()
+
+
+
 
 
 
